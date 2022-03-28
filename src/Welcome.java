dfd58001267e52ca92db3e65a7034da293c6c0ac
xixiaohui0513 <1664938460@qq.com>
2022-03-28 22:26:35 +0800

    Initial commit

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome {
+    //这是单行注释  main()是程序的入口
+    public static void main(String[] args) {
+        System.out.println("席小辉");
+		System.out.println("要说技术哪家强");
+		System.out.println("当属中国尚学堂");
+		System.out.println("学堂一支穿云箭");
+		System.out.println("千军万马来相见");
+
+    }
+
+}
